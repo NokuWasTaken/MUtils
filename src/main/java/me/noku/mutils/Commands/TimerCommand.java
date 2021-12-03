2c1a287cbb788ed88d284694e89bd0a1faa3c075
Noku <NokuWasTakenWasTaken@gmail.com>
2021-12-03 23:46:50 +0100

    Added GUI's to /Settings, /Life, /Timer

--- src/main/java/me/noku/mutils/Commands/TimerCommand.java
+++ src/main/java/me/noku/mutils/Commands/TimerCommand.java
@@ -1,6 +1,7 @@
 package me.noku.mutils.Commands;
 
 
+import me.noku.mutils.GUI.LifeMenu;
 import me.noku.mutils.MUtils;
 import me.noku.mutils.UTils.Timer;
 import org.bukkit.Bukkit;
@@ -14,44 +15,48 @@ import org.jetbrains.annotations.NotNull;
 public class TimerCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        for (Player pp : Bukkit.getOnlinePlayers()) {
-            switch (args[0]) {
-                case "resume":
-                    Timer timer = MUtils.getInstance().getTimer();
-                    if (!timer.isRunning()) {
-                        timer.setRunning(true);
-                        pp.sendMessage(MUtils.TimerPrefix() + ChatColor.GOLD + "Der Timer wurde " + ChatColor.GREEN + "gestartet");
-                        MUtils.getInstance().ProtTime();
-                    }
-                    break;
-                case "pause":
-                    Timer timer2 = MUtils.getInstance().getTimer();
-                    if (timer2.isRunning()) {
-                        timer2.setRunning(false);
-                        pp.sendMessage(MUtils.TimerPrefix() + ChatColor.GOLD + "Der Timer wurde " + ChatColor.RED + "pausiert");
-                    }
-                    break;
-                case "reset":
-                    Timer timer3 = MUtils.getInstance().getTimer();
-                    timer3.setTime(0);
-                    timer3.setRunning(false);
-                pp.sendMessage(MUtils.TimerPrefix() + ChatColor.GOLD + "Der Timer wurde " + ChatColor.DARK_RED + "Zurückgesetzt");
-                    break;
-                case "set":
-                    Timer timer4 = MUtils.getInstance().getTimer();
-                    if (args.length == 2) {
-                        timer4.setTime(Integer.parseInt(args[1]));
-                        timer4.setRunning(false);
-                        pp.sendMessage(MUtils.TimerPrefix() + ChatColor.GOLD + "Der Timer wurde auf " + ChatColor.AQUA + args[1] + ChatColor.GOLD + " Gesetzt");
-                    }
-                    break;
-
-
+        if (args.length == 1) {
+            for (Player pp : Bukkit.getOnlinePlayers()) {
+                switch (args[0]) {
+                    case "resume":
+                        Timer timer = MUtils.getInstance().getTimer();
+                        if (!timer.isRunning()) {
+                            timer.setRunning(true);
+                            pp.sendMessage(MUtils.TimerPrefix() + ChatColor.GOLD + "Der Timer wurde " + ChatColor.GREEN + "gestartet");
+                            MUtils.getInstance().ProtTime();
+                        }
+                        break;
+                    case "pause":
+                        Timer timer2 = MUtils.getInstance().getTimer();
+                        if (timer2.isRunning()) {
+                            timer2.setRunning(false);
+                            pp.sendMessage(MUtils.TimerPrefix() + ChatColor.GOLD + "Der Timer wurde " + ChatColor.RED + "pausiert");
+                        }
+                        break;
+                    case "reset":
+                        Timer timer3 = MUtils.getInstance().getTimer();
+                        timer3.setTime(0);
+                        timer3.setRunning(false);
+                        pp.sendMessage(MUtils.TimerPrefix() + ChatColor.GOLD + "Der Timer wurde " + ChatColor.DARK_RED + "Zurückgesetzt");
+                        break;
+                    case "set":
+                        Timer timer4 = MUtils.getInstance().getTimer();
+                        if (args.length == 2) {
+                            timer4.setTime(Integer.parseInt(args[1]));
+                            timer4.setRunning(false);
+                            pp.sendMessage(MUtils.TimerPrefix() + ChatColor.GOLD + "Der Timer wurde auf " + ChatColor.AQUA + args[1] + ChatColor.GOLD + " Gesetzt");
+                        }
+                        break;
+                }
             }
-
-
-
+        } else if (args.length == 0) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                LifeMenu.LifeGenerator();
+                player.openInventory(LifeMenu.gui);
+            }
         }
+
 
         return false;
     }
