8e4003ba9d79f986c0b8ed5cb7017affb8d77ec9
Noku <NokuWasTakenWasTaken@gmail.com>
2021-12-04 00:49:53 +0100

    bugfixing

--- src/main/java/me/noku/mutils/Events/DamageEvent.java
+++ src/main/java/me/noku/mutils/Events/DamageEvent.java
@@ -13,37 +13,45 @@ import org.bukkit.potion.PotionEffectType;
 public class DamageEvent implements Listener {
 
     @EventHandler
-    public void onDamage (EntityDamageEvent event) {
+    public void onDamage(EntityDamageEvent event) {
         if (event.getEntity() instanceof Player) {
             Player player = (Player) event.getEntity();
             if (!MUtils.getInstance().getTimer().isRunning()) {
                 event.setCancelled(true);
                 return;
             }
+
             if (MUtils.getInstance().getTimer().isRunning()) {
                 event.setDamage(event.getDamage() * MUtils.getInstance().getConfig().getInt("gamerule.damageMultiplier"));
             }
 
-            if (MUtils.getInstance().getConfig().getBoolean("gamerule.splitHearts")) {
-                for (Player allPlayer : Bukkit.getOnlinePlayers()) {
-                    allPlayer.setHealth(allPlayer.getHealth() - event.getDamage());
+            if (MUtils.getInstance().getTimer().isRunning()) {
+                if (MUtils.getInstance().getConfig().getBoolean("gamerule.splitHearts")) {
+                    for (Player allPlayer : Bukkit.getOnlinePlayers()) {
+                        allPlayer.setHealth(allPlayer.getHealth() - event.getDamage());
+                    }
                 }
             }
 
-            if (MUtils.getInstance().getConfig().getBoolean("gamerule.damageMessage")) {
-                if (MUtils.getInstance().getTimer().isRunning()) {
+            if (MUtils.getInstance().getTimer().isRunning()) {
+                if (MUtils.getInstance().getConfig().getBoolean("gamerule.50BlocksIntoTheAir")) {
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 1 + 20, 42, false));
+                }
+            }
+
+            if (MUtils.getInstance().getTimer().isRunning()) {
+                if (MUtils.getInstance().getConfig().getBoolean("gamerule.damageMessage")) {
                     for (Player allPlayer : Bukkit.getOnlinePlayers()) {
                         allPlayer.sendMessage(MUtils.DamagePrefix() + ChatColor.BLUE + ((Player) event.getEntity()).getDisplayName() + ChatColor.GOLD + " hat " + ChatColor.RED + event.getDamage() / 2 + " Herzen " + ChatColor.GRAY + "[ " + ChatColor.AQUA + event.getCause() + ChatColor.GRAY + " ] " + ChatColor.GOLD + "Schaden erhalten");
                     }
                 }
             }
 
-            if (event.getCause() == EntityDamageEvent.DamageCause.FALL) {
-                if (MUtils.getInstance().getConfig().getBoolean("gamerule.noFallDamage")) {
-                    player.setHealth(0);
-                }
-                if (MUtils.getInstance().getConfig().getBoolean("gamerule.50BlocksIntoTheAir")) {
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 1, 42, false));
+            if (MUtils.getInstance().getTimer().isRunning()) {
+                if (event.getCause() == EntityDamageEvent.DamageCause.FALL) {
+                    if (MUtils.getInstance().getConfig().getBoolean("gamerule.noFallDamage")) {
+                        player.setHealth(0);
+                    }
                 }
             }
         }
