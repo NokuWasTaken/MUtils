3799fe290656ccd70132739ba81687e7e7ea4d45
Noku <NokuWasTakenWasTaken@gmail.com>
2021-12-03 23:46:29 +0100

    Added GUI's to /Settings, /Life, /Timer

--- src/main/java/me/noku/mutils/Commands/LifeCommand.java
+++ src/main/java/me/noku/mutils/Commands/LifeCommand.java
@@ -1,21 +1,21 @@
 package me.noku.mutils.Commands;
 
-import me.noku.mutils.GUI.SettingsMenu;
+import me.noku.mutils.GUI.LifeMenu;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import sun.awt.XSettings;
+import sun.nio.ch.sctp.SendFailed;
 
-public class SettingsCommand implements CommandExecutor {
+public class LifeCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (args.length == 0) {
-                SettingsMenu.SettingsGenerator();
-                player.openInventory(SettingsMenu.gui);
+                LifeMenu.LifeGenerator();
+                player.openInventory(LifeMenu.gui);
             }
         }
         return false;
