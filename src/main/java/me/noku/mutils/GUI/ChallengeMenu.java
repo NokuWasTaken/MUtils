5adff3f19e3c6cdd53b94e3368948e9f6d5adbc9
Noku <NokuWasTakenWasTaken@gmail.com>
2021-12-03 23:45:24 +0100

    Added BlockDespawn, OnlyDirt and BedrockWalls Challenge

---
+++ src/main/java/me/noku/mutils/GUI/ChallengeMenu.java
@@ -0,0 +1,24 @@
+package me.noku.mutils.GUI;
+
+import me.noku.mutils.UTils.ItemBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class ChallengeMenu {
+    public static Inventory gui = Bukkit.createInventory(null, 36, ChatColor.GRAY + ">> " + ChatColor.GOLD + "Challenges");
+
+    public static void InventoryBuilder () {
+        for (int i = 0; i <=35; i++) {
+            gui.setItem(i, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setDisplayname("").build());
+        }
+        gui.setItem(10, new ItemBuilder(Material.DIRT).setDisplayname(ChatColor.GOLD + "Only Dirt").setLore("Click / Set").build());
+        gui.setItem(19, new ItemBuilder(Material.BEDROCK).setDisplayname("Bedrock Walls").setLore("Click / Set").build());
+        gui.setItem(11, new ItemBuilder(Material.BARRIER).setDisplayname("Blöcke verschwinden").setLore("Click / Set").build());
+
+
+    }
+
+}
---
+++ src/main/java/me/noku/mutils/GUIHandler/ChallengeMenuHandler.java
@@ -0,0 +1,75 @@
+package me.noku.mutils.GUIHandler;
+
+import me.noku.mutils.MUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class ChallengeMenuHandler implements Listener {
+    @EventHandler
+    public void onClick(InventoryClickEvent event) {
+        if (event.getWhoClicked() instanceof Player) {
+            Player player = (Player) event.getWhoClicked();
+            if (event.getView().getTitle().equals(ChatColor.GRAY + ">> " + ChatColor.GOLD + "Life Settings")) {
+                if (event.getCurrentItem() != null) {
+                    if (event.getCurrentItem().getItemMeta() != null) {
+                        event.setCancelled(true);
+                        switch (event.getSlot()) {
+                            case 10:
+                                if (MUtils.getInstance().getConfig().getBoolean("challenge.onlyDirt")) {
+                                    MUtils.getInstance().getConfig().set("challenge.onlyDirt", false);
+                                    MUtils.getInstance().saveConfig();
+                                    for (Player allPlayer : Bukkit.getOnlinePlayers()) {
+                                        allPlayer.sendMessage(MUtils.MainPrefix() + ChatColor.RED + "OnlyDirt challenge deaktiviert");
+                                    }
+                                } else if (!MUtils.getInstance().getConfig().getBoolean("challenge.onlyDirt")) {
+                                    MUtils.getInstance().getConfig().set("challenge.onlyDirt", true);
+                                    MUtils.getInstance().saveConfig();
+                                    for (Player allPlayer : Bukkit.getOnlinePlayers()) {
+                                        allPlayer.sendMessage(MUtils.MainPrefix() + ChatColor.GREEN + "OnlyDirt Challenge aktiviert");
+                                    }
+                                }
+                                break;
+                            case 19:
+                                if (MUtils.getInstance().getConfig().getBoolean("challenge.bedrockWalls")) {
+                                    MUtils.getInstance().getConfig().set("challenge.bedrockWalls", false);
+                                    MUtils.getInstance().saveConfig();
+                                    for (Player allPlayer : Bukkit.getOnlinePlayers()) {
+                                        allPlayer.sendMessage(MUtils.MainPrefix() + ChatColor.RED + "BedrockWalls challenge deaktiviert");
+                                    }
+                                } else if (!MUtils.getInstance().getConfig().getBoolean("challenge.bedrockWalls")) {
+                                    MUtils.getInstance().getConfig().set("challenge.bedrockWalls", true);
+                                    MUtils.getInstance().saveConfig();
+                                    for (Player allPlayer : Bukkit.getOnlinePlayers()) {
+                                        allPlayer.sendMessage(MUtils.MainPrefix() + ChatColor.GREEN + "BedrockWalls Challenge aktiviert");
+                                    }
+                                }
+                                break;
+                            case 11:
+                                if (MUtils.getInstance().getConfig().getBoolean("challenge.blockDespawn")) {
+                                    MUtils.getInstance().getConfig().set("challenge.blockDespawn", false);
+                                    MUtils.getInstance().saveConfig();
+                                    for (Player allPlayer : Bukkit.getOnlinePlayers()) {
+                                        allPlayer.sendMessage(MUtils.MainPrefix() + ChatColor.RED + "BlockDespawn challenge deaktiviert");
+                                    }
+                                } else if (!MUtils.getInstance().getConfig().getBoolean("challenge.blockDespawn")) {
+                                    MUtils.getInstance().getConfig().set("challenge.blockDespawn", true);
+                                    MUtils.getInstance().saveConfig();
+                                    for (Player allPlayer : Bukkit.getOnlinePlayers()) {
+                                        allPlayer.sendMessage(MUtils.MainPrefix() + ChatColor.GREEN + "BlockDespawn Challenge aktiviert");
+                                    }
+                                }
+                                break;
+
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
