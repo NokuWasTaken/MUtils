1489e50448744c1f1cb1e665759abd418b944b61
NokuWasTaken <NokuWasTakenWasTaken@gmail.com>
2021-12-11 03:50:46 +0100

    Q.o.L chnages

--- src/main/java/me/noku/mutils/GUI/MainMenu.java
+++ src/main/java/me/noku/mutils/GUI/MainMenu.java
@@ -8,18 +8,18 @@ import org.bukkit.inventory.Inventory;
 
 public class MainMenu {
 
-    public static Inventory gui = Bukkit.createInventory( null, 45, ChatColor.GRAY + ">> "+ ChatColor.GOLD + ChatColor.BOLD + "MUtils" );
+    public static Inventory gui = Bukkit.createInventory( null, 54, ChatColor.GRAY + ">> "+ ChatColor.GOLD + ChatColor.BOLD + "MUtils" );
 
     public static void MainGUIGenerator() {
-        for (int i = 0; i <= 44; i++) {
+        for (int i = 0; i <= 53; i++) {
             gui.setItem(i, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setDisplayname("").build());
         }
 
-        gui.setItem(13, new ItemBuilder(Material.NETHER_STAR).setDisplayname(ChatColor.GOLD + "MUtils " + ChatColor.AQUA + "v1.4.1").build());
-        gui.setItem(28, new ItemBuilder(Material.CONDUIT).setDisplayname(ChatColor.DARK_PURPLE + "Challenges").setLore("Click / Open").build());
-        gui.setItem(30, new ItemBuilder(Material.COMMAND_BLOCK).setDisplayname(ChatColor.GOLD + "Settings").setLore("Click / Open").build());
-        gui.setItem(32, new ItemBuilder(Material.CLOCK).setDisplayname(ChatColor.GREEN + "Timer").setLore("Click / Open").build());
-        gui.setItem(34, new ItemBuilder(Material.GOLDEN_APPLE).setDisplayname(ChatColor.RED + "Life").setLore("Click / Open").build());
+        gui.setItem(13, new ItemBuilder(Material.NETHER_STAR).setDisplayname(ChatColor.GOLD + "MUtils " + ChatColor.AQUA + "v1.4.2").build());
+        gui.setItem(37, new ItemBuilder(Material.CONDUIT).setDisplayname(ChatColor.DARK_PURPLE + "Challenges").setLore("Click / Open").build());
+        gui.setItem(39, new ItemBuilder(Material.COMMAND_BLOCK).setDisplayname(ChatColor.GOLD + "Settings").setLore("Click / Open").build());
+        gui.setItem(41, new ItemBuilder(Material.CLOCK).setDisplayname(ChatColor.GREEN + "Timer").setLore("Click / Open").build());
+        gui.setItem(43, new ItemBuilder(Material.GOLDEN_APPLE).setDisplayname(ChatColor.RED + "Life").setLore("Click / Open").build());
     }
 
 }
